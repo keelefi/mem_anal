f8167b4d63a4f53fefa9059cf4f8a8aa043dcabc
Michele Lindroos <michele.lindroos@gmail.com>
2016-08-03 01:54:14 +0300

    add basic trace{e,r} functions

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <system_error>
+#include <vector>
 
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <assert.h>
 #include <errno.h>
+#include <signal.h>
 #include <unistd.h>
 
 void print_usage(const char *argv0)
@@ -11,6 +17,40 @@ void print_usage(const char *argv0)
             << std::string(argv0)
             << " <command>"
             << std::endl;
+}
+
+void tracer_func(pid_t child_pid)
+{
+    int status;
+    if (waitpid(child_pid, &status, 0) == -1)
+    {
+        throw std::system_error(errno, std::system_category());
+    }
+    assert(WIFSTOPPED(status));
+}
+
+void tracee_func(int argc, char **argv)
+{
+    // we need to remove argv[0] and end the array with a NULL element
+    std::vector<char *> child_argv;
+    for (int i = 1; i < argc; ++i)
+    {
+        child_argv.push_back(argv[i]);
+    }
+
+    if (ptrace(PTRACE_TRACEME) == -1)
+    {
+        throw std::system_error(errno, std::system_category());
+    }
+
+    if (kill(getpid(), SIGSTOP) == -1)
+    {
+        throw std::system_error(errno, std::system_category());
+    }
+
+    // execvp() returns only if an error occured
+    execvp(child_argv[0], child_argv.data());
+    throw std::system_error(errno, std::system_category());
 }
 
 int main(int argc, char **argv)
@@ -29,11 +69,11 @@ int main(int argc, char **argv)
     }
     else if (child_pid == 0)
     {
-        std::cout << "hello from child" << std::endl;
+        tracee_func(argc, argv);
     }
     else
     {
-        std::cout << "hello from parent" << std::endl;
+        tracer_func(child_pid);
     }
 
     return 0;

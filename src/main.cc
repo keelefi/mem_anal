6eda33bb9451332f7cb5b8b14cbaa2f7fc3899e3
Michele Lindroos <michele.lindroos@gmail.com>
2016-08-03 00:29:12 +0300

    add forking

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <system_error>
+
+#include <errno.h>
+#include <unistd.h>
 
 void print_usage(const char *argv0)
 {
@@ -11,7 +15,26 @@ void print_usage(const char *argv0)
 
 int main(int argc, char **argv)
 {
-    print_usage(argv[0]);
+    if (argc < 2)
+    {
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    pid_t child_pid = fork();
+
+    if (child_pid < 0)
+    {
+        throw std::system_error(errno, std::system_category());
+    }
+    else if (child_pid == 0)
+    {
+        std::cout << "hello from child" << std::endl;
+    }
+    else
+    {
+        std::cout << "hello from parent" << std::endl;
+    }
 
     return 0;
 }

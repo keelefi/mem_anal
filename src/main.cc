07b3dca590462caa2579e468e4d8aa158a0a5c48
Michele Lindroos <michele.lindroos@gmail.com>
2016-08-03 00:11:27 +0300

    add print_usage()

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,17 @@
+#include <iostream>
+
+void print_usage(const char *argv0)
+{
+    std::cout
+            << "usage: "
+            << std::string(argv0)
+            << " <command>"
+            << std::endl;
+}
+
 int main(int argc, char **argv)
 {
+    print_usage(argv[0]);
+
     return 0;
 }
